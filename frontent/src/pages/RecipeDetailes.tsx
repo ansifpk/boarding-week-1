81ed27a617472eea4fe4bb03268f82154e74931f
ansifpk <pkansif39@gmail.com>
2025-02-14 23:05:04 +0530

    changed the api key of spoonacular website

--- frontent/src/pages/RecipeDetailes.tsx
+++ frontent/src/pages/RecipeDetailes.tsx
@@ -1,7 +1,22 @@
 import Header from '@/components/Header'
-import React from 'react'
+import { IRecipe } from '@/lib/types'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 const RecipeDetailes = () => {
+  const [recipe,setRecipe] = useState<IRecipe>()
+  const [recipeId] = useSearchParams()
+ const id =  recipeId.get('recipeId')
+  useEffect(()=>{
+    const fetching = async()=>{
+         const {data} = await axios.get(`${import.meta.env.VITE_SPOONACULAR_API}/${id}/information?includeNutrition=true&apiKey=${import.meta.env.VITE_SPOONACULAR_API_KEY}`)
+         setRecipe(data)
+        }
+    fetching()
+  },[id])
+  console.log(recipe);
+  
   return (
     <div className='bg-pink-50'>
        <Header />
@@ -10,7 +25,7 @@ const RecipeDetailes = () => {
        <div className="flex gap-5 mt-5">
          <div className='w-75 '>
             <div className='font-semibold text-4xl font-serif'>
-            The Best Soft Chocolate  Chip Cookies
+               The Best Soft Chocolate  Chip Cookies
             </div>
             <div className='h-[400px] w-full'>
               <img src={`https://img.spoonacular.com/recipes/639333-636x393.jpg`} alt={"recipe.title"} className='w-full h-full object-cover' />
