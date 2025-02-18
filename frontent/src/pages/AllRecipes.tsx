c88176b2ca0c2947051f8c96ec2397206556f11d
ansifpk <pkansif39@gmail.com>
2025-02-19 01:25:22 +0530

    added border

--- frontent/src/pages/AllRecipes.tsx
+++ frontent/src/pages/AllRecipes.tsx
@@ -9,7 +9,15 @@ import {
   PaginationLink,
   PaginationNext,
 } from "@/components/ui/pagination";
-import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { IRecipe } from "@/lib/types";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -17,31 +25,29 @@ import { useEffect, useState } from "react";
 const AllRecipes = () => {
   const [count, setCount] = useState(1);
   const [pages] = useState(5229);
-  const [sort,setSort] = useState('All');
+  const [sort, setSort] = useState("All");
   const [recipe, setRecipe] = useState<IRecipe[]>([]);
   useEffect(() => {
     const fetching = async () => {
-      let url = `${
-        import.meta.env.VITE_SPOONACULAR_API
-      }/complexSearch?offset=${count * 9}&number=9&apiKey=${
-        import.meta.env.VITE_SPOONACULAR_API_KEY
-      }`;
+      let url = `${import.meta.env.VITE_SPOONACULAR_API}/complexSearch?offset=${
+        count * 9
+      }&number=9&apiKey=${import.meta.env.VITE_SPOONACULAR_API_KEY}`;
       if (sort !== "All") {
-        url += `&${sort}`; 
+        url += `&${sort}`;
       }
 
       const { data } = await axios.get(url);
       setRecipe(data.results);
     };
-    fetching()
+    fetching();
   }, [count]);
 
-  if(!recipe.length){
-    return(
+  if (!recipe.length) {
+    return (
       <>
-        <BrickLoader/>
+        <BrickLoader />
       </>
-    )
+    );
   }
   return (
     <div className="bg-pink-50">
@@ -50,59 +56,69 @@ const AllRecipes = () => {
         ALL RECIPES
       </div>
       <div className="flex">
-         {/* <div> */}
-         <Select  onValueChange={(value) => {
+        {/* <div> */}
+        <Select
+          onValueChange={(value) => {
             setSort(value);
-          }} defaultValue={sort}>
-      <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Select a fruit" />
-      </SelectTrigger>
-      <SelectContent >
-        <SelectGroup >
-          <SelectLabel>Sort</SelectLabel>
-          <SelectItem value="All">All</SelectItem>
-          <SelectItem value="sort=price&sortDirection=desc">Price High</SelectItem>
-          <SelectItem value="sort=price&sortDirection=asc">Price Low</SelectItem>
-          <SelectItem value="sort=readyInMinutes&sortDirection=asc">Coking Time Fast</SelectItem>
-          {/* <SelectItem value="grapes">Popularity High</SelectItem> */}
-        </SelectGroup>
-      </SelectContent>
-    </Select>
-         {/* </div> */}
-         <div className="w-full">
-        {/* body part start */}
-        <Cards arr={recipe} />
-        {/* body part end  */}
+          }}
+          defaultValue={sort}
+          
+        >
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select a fruit" />
+          </SelectTrigger>
+          <SelectContent className="border">
+            <SelectGroup>
+              <SelectLabel>Sort</SelectLabel>
+              <SelectItem value="All">All</SelectItem>
+              <SelectItem value="sort=price&sortDirection=desc">
+                Price High
+              </SelectItem>
+              <SelectItem value="sort=price&sortDirection=asc">
+                Price Low
+              </SelectItem>
+              <SelectItem value="sort=readyInMinutes&sortDirection=asc">
+                Coking Time Fast
+              </SelectItem>
+              {/* <SelectItem value="grapes">Popularity High</SelectItem> */}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+        {/* </div> */}
+        <div className="w-full">
+          {/* body part start */}
+          <Cards arr={recipe} />
+          {/* body part end  */}
 
-        {/* pagination start */}
-        <Pagination>
-          <PaginationContent>
-            <PaginationItem>
-              <PaginationPrevious
-                onClick={() => {
-                  count > 1 ? setCount(count - 1) : setCount(count);
-                }}
-                className="text-black cursor-pointer"
-              />
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationLink className="text-black cursor-pointer" isActive>
-                {count}
-              </PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationNext
-                onClick={() => {
-                  count !== pages ? setCount(count + 1) : setCount(count);
-                }}
-                className="text-black cursor-pointer"
-              />
-            </PaginationItem>
-          </PaginationContent>
-        </Pagination>
+          {/* pagination start */}
+          <Pagination>
+            <PaginationContent>
+              <PaginationItem>
+                <PaginationPrevious
+                  onClick={() => {
+                    count > 1 ? setCount(count - 1) : setCount(count);
+                  }}
+                  className="text-black cursor-pointer"
+                />
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationLink className="text-black cursor-pointer" isActive>
+                  {count}
+                </PaginationLink>
+              </PaginationItem>
+              <PaginationItem>
+                <PaginationNext
+                  onClick={() => {
+                    count !== pages ? setCount(count + 1) : setCount(count);
+                  }}
+                  className="text-black cursor-pointer"
+                />
+              </PaginationItem>
+            </PaginationContent>
+          </Pagination>
 
-        {/* pagination end */}
-      </div>
+          {/* pagination end */}
+        </div>
       </div>
     </div>
   );
